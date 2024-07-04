155efc6f34050428a060d3f270cd030fdd19b616
Rick Gao <wcdaren@gmail.com>
2024-07-04 23:17:49 +0800

    2024-07-04

--- apps/notifications-service/src/main.ts
+++ apps/notifications-service/src/main.ts
@@ -1,24 +1,24 @@
-import { NestFactory } from '@nestjs/core'
-import { ValidationPipe } from '@nestjs/common'
-import { MicroserviceOptions, Transport } from '@nestjs/microservices'
-import { NotificationsServiceModule } from './notifications-service.module'
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import { MicroserviceOptions, Transport } from "@nestjs/microservices";
+import { NotificationsServiceModule } from "./notifications-service.module";
 
 async function bootstrap() {
-	const app = await NestFactory.create(NotificationsServiceModule)
-	app.useGlobalPipes(new ValidationPipe())
-	app.connectMicroservice<MicroserviceOptions>(
-		{
-			transport: Transport.RMQ,
-			options: {
-				urls: [process.env.RABBITMQ_URL],
-				queue: 'notifications-service',
-				noAck: false
-			}
-		},
-		{ inheritAppConfig: true }
-	)
-	await app.startAllMicroservices()
-	await app.listen(3000)
+  const app = await NestFactory.create(NotificationsServiceModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URL],
+        queue: "notifications-service",
+        noAck: false,
+      },
+    },
+    { inheritAppConfig: true }
+  );
+  await app.startAllMicroservices();
+  await app.listen(3000);
 }
 
-bootstrap()
+bootstrap();
